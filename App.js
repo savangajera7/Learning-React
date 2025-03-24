5a6a1a5429961e8484b0344e90124835d13cd515
savangajera7 <gajerasavan7@gmail.com>
2025-03-24 11:46:15 +0530

    episode-01

--- App.js
+++ App.js
@@ -15,20 +15,20 @@
  */
 
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-]);
-
-//JSX
-
-console.log(parent); // object
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+    React.createElement("div", { id: "child" }, [
+      React.createElement("h1", {}, "I'm an h1 tag"),
+      React.createElement("h2", {}, "I'm an h2 tag"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+      React.createElement("h1", {}, "I'm an h1 tag"),
+      React.createElement("h2", {}, "I'm an h2 tag"),
+    ]),
+  ]);
+  
+  //JSX
+  
+  console.log(parent); // object
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  
+  root.render(parent);
